19fc4eda5796d33de22499b8f86642fa838fc750
Mysski0m <pomaksim@icloud.com>
2025-10-10 21:37:32 +0300

    add lab2_var11

---
+++ lab2_var11/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include "thirteen_num_syst.h"
+#include "exceptions.h"
+
+int main() {
+    std::cout << "Enter number to show" << std::endl;
+    std::string input;
+    std::cin >> input;
+    try {
+        thirteen::Thirteen number(input);
+        std::cout << number.Show() << std::endl;
+    } catch (exception::ExceptionNull) {
+        std::cout << "Wrong input: not a number in thirteen numerical system" << std::endl;
+    }
+    return 0;
+}
